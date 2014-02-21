9d34cc2e1e1c5ec9905b711af36a35c202023517
Nicolas Ritouet <nicolas.ritouet@zanox.com>
2014-02-21 17:15:21 +0100

    Add UI, backend routes and improve logic

---
+++ example.config.js
@@ -0,0 +1,10 @@
+var config = {}
+config.blockchain = {};
+
+config.blockchain.password = 'xxxxxxxxxxxxx';
+config.blockchain.guid = 'xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx';
+config.blockchain.address = '1CK5cgFeJCoHyjvxUjRTfqWBL74fUqdb9G';
+config.blockchain.wsUrl = 'wss://ws.blockchain.info/inv';
+config.blockchain.restApiUrl = 'https://blockchain.info/merchant/';
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,20 +1,59 @@
-var koa = require('koa'),
+// process.env.DEBUG = '.*';
+/**
+* Module dependencies.
+*/
+var _ = require('lodash-node'),
+    views = require('co-views'),
+    serve = require('koa-static'),
+    route = require('koa-route'),
+    koa = require('koa'),
     app = koa(),
     request = require('request'),
+    WebSocket = require('ws'),
+    debug = require('debug')('bitcoinVendingMachine'),
     config = require('./config');
 
-    var baseBlockchainUrl = 'https://blockchain.info/merchant/';
+var render = views(__dirname + '/views', { ext: 'ejs' });
 
 
-app.use(function *() {
-    var response = yield doHttpRequest(baseBlockchainUrl + config.blockchain.guid + '/balance?password=' + config.blockchain.password);
-    var balance = JSON.parse(response.body).balance;
-    this.body = "Current balance of address 1CK5cgFeJCoHyjvxUjRTfqWBL74fUqdb9G: " + balance + " Satoshi";
-});
+/**
+* Routing.
+*/
+app.use(serve('public'));
+app.use(route.get('/', main));
+app.use(route.get('/api', api));
+// app.use(route.get('/balance', balance));
+// app.use(route.get('/exchangeRate', exchangeRate));
 
-function *doHttpRequest(url) {
-    var resultParams = yield doRequest(url);
-    return resultParams[0];
+
+function *main() {
+
+    this.body = yield render('main', {
+        exchangeRate: yield getExchangeRate(),
+        balance: yield getBalance(),
+        timestamp: new Date,
+        btcAddress: config.blockchain.address
+    });
+}
+
+function *api() {
+
+    this.body = {
+        timestamp: Date.now(),
+        balance: yield getBalance(),
+        exchangeRate: yield getExchangeRate(),
+        btcAddress: config.blockchain.address
+    };
+}
+
+function *getBalance() {
+    var response = yield doRequest(config.blockchain.restApiUrl + config.blockchain.guid + '/balance?password=' + config.blockchain.password);
+    return JSON.parse(response[0].body).balance;
+}
+
+function *getExchangeRate() {
+    var responseExchangeRate = yield doRequest('http://blockchain.info/tobtc?currency=EUR&value=1');
+    return responseExchangeRate[0].body;
 }
 
 function doRequest(url) {
@@ -23,4 +62,31 @@ function doRequest(url) {
     }
 }
 
-app.listen(3000);
+var ws = new WebSocket(config.blockchain.wsUrl);
+ws.on('open', function () {
+    debug('connected to:', config.blockchain.wsUrl);
+    debug("Subscribed to transaction event for address " + config.blockchain.address);
+    ws.send(JSON.stringify({"op": "addr_sub", "addr": config.blockchain.address}));
+});
+
+ws.on('message', function (data) {
+    var transaction = JSON.parse(data);
+    var transactionValueReceived = getTransValueForAddress(transaction, config.blockchain.address);
+    debug(new Date() + " - I received " + transactionValueReceived + " Satoshis.");
+});
+
+ws.on('error', function (err) {
+    debug(err);
+});
+
+var getTransValueForAddress = function(transaction, btcAddress) {
+    var outArray = transaction.x.out;
+    var outTx = (_.find(outArray, function(outTx) {
+        return outTx.addr === btcAddress;
+    }));
+    return outTx.value;
+
+}
+
+
+app.listen(3001);
